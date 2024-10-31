2638580f1a0e24da010962601656cf0fec2bf2c7
Lina Naranjo <lina.naja1807@gmail.com>
2024-10-31 15:17:50 -0500

    Inicios del loguin

--- src/componentes/FormLoguin.jsx
+++ src/componentes/FormLoguin.jsx
@@ -3,9 +3,28 @@ import React from "react";
 
 const FormLogin = () => {
   return (
-    <div>
-      <p>Bienvenidos a Piggy la aplicación mas divertida</p>
-    </div>
+    <>
+      <h1>Ingresar</h1>
+      <div>
+        <form action="">
+          <section>
+            <label htmlFor="">Correo Electrónico</label>
+            <figure></figure>
+            <input type="text" placeholder="example@example.com" />
+          </section>
+          <section>
+            <label htmlFor="">Contraseña</label>
+            <figure></figure>
+            <input type="text" placeholder="*******" />
+          </section>
+          <div>
+            <p>Olviaste la contraseña ?</p>
+            <button>Iniciar Sesión</button>
+          </div>
+        </form>
+        <p>¿ No tienes cuenta ? ¡Regístrate!</p>
+      </div>
+    </>
   );
 };
 export default FormLogin;
