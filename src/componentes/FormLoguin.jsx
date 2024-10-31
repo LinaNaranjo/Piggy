93828bb3f0b21d48662afcbf00feae78a7cb57c8
Lina Naranjo <lina.naja1807@gmail.com>
2024-10-31 11:54:10 -0500

    Primer Commit: Inicio del proyecto

---
+++ src/componentes/FormLoguin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import "./formLoguin.scss"
+
+const FormLogin = () => {
+  return (
+    <div>
+      <p>Bienvenidos a Piggy</p>
+    </div>
+  );
+};
+export default FormLogin;
