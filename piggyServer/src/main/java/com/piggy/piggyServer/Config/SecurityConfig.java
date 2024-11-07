9f1da359c02b6f01e5f1142c623dd15dfc0dcb12
Santiago Garcia <santi10gz10@gmail.com>
2024-11-07 13:32:22 -0500

    security config

---
+++ piggyServer/src/main/java/com/piggy/piggyServer/Config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.piggy.piggyServer.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .csrf(csrf -> csrf.disable()) //Deshabilitar la protección CSRF en Spring Security.
+        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+            .requestMatchers("/prueba").permitAll()
+            .anyRequest().authenticated())
+        .build();
+  }
+}
