4d0fda83e80130f688445b3e4370fe76db2f1ad8
Santiago Garcia <santi10gz10@gmail.com>
2024-11-06 23:10:15 -0500

    Estructura de carpetas

---
+++ piggyServer/src/main/java/com/piggy/piggyServer/PiggyServerApplication.java
@@ -0,0 +1,13 @@
+package com.piggy.piggyServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiggyServerApplication.class, args);
+	}
+
+}
---
+++ piggyServer/src/test/java/com/piggy/piggyServer/PiggyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piggy.piggyServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiggyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
