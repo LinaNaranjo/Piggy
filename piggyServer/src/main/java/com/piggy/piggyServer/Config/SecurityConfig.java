0f78ef56455eea2de1daa9a2c6a48d28063c4739
Santiago Garcia <santi10gz10@gmail.com>
2024-11-07 21:16:04 -0500

    first push

--- piggyServer/src/main/java/com/piggy/piggyServer/Config/SecurityConfig.java
+++ piggyServer/src/main/java/com/piggy/piggyServer/Config/SecurityConfig.java
@@ -11,12 +11,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-  @Bean
+  @Bean // es un objeto gestionado por el contenedor de Spring, lo que significa que Spring crea y gestiona la instancia de esta clase.
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
         .csrf(csrf -> csrf.disable()) //Deshabilitar la protección CSRF en Spring Security.
-        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-            .requestMatchers("/prueba").permitAll()
+        .authorizeHttpRequests(authorizeRequests -> authorizeRequests //regla de autorización, como se deben manejar las solicitudes dependiendo de la ruta
+            .requestMatchers("/prueba").permitAll() //prueba debe ser permitida sin necesidad de autenticación. Es decir, cualquiera podrá acceder a esta ruta sin estar autenticado.
             .anyRequest().authenticated())
         .build();
   }
