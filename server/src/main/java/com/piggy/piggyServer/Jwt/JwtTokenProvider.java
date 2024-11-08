7337fcff99e078f169895cbad46f85c0bb41a0c0
Santiago Garcia <santi10gz10@gmail.com>
2024-11-08 11:52:03 -0500

    inicios back a developer

--- server/src/main/java/com/piggy/piggyServer/Jwt/JwtTokenProvider.java
+++ server/src/main/java/com/piggy/piggyServer/Jwt/JwtTokenProvider.java
@@ -2,15 +2,18 @@ package com.piggy.piggyServer.Jwt;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 
 @Service
 public class JwtTokenProvider {
-  private final String jwtSecret = "QnFybGZMVzJmbkpFclpGQ3NyZzVZSkpINzkwbkVzWTM=";
+  private final String secretKey = "QnFybGZMVzJmbkpFclpGQ3NyZzVZSkpINzkwbkVzWTM=";
   public String getToken(UserDetails userDetails){
     return getToken(new HashMap<>(), userDetails);
   }
@@ -22,11 +25,13 @@ public class JwtTokenProvider {
         .setSubject(userDetails.getUsername())//TODO validar si es getusername o getemail
         .setIssuedAt(new Date(System.currentTimeMillis())) //fecha de creacion del token
         .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) //10 horas de expiracion
-        .signWith(getKey(),SignatureAlgorithm.HS256)
+        .signWith(getKey(),SignatureAlgorithm.HS256)//getkey: proporciona la clave secreta - HS256: algoritmo de encriptacion
         .compact();
   }
 
-  private SignatureAlgorithm getKey() {
+  private Key getKey() {
+    byte[]keyBytes = Decoders.BASE64.decode(secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
   }
 
 }
