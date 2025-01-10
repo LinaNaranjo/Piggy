d4ceb0fcb33f7d280219ef45bc512eb42f64a245
AlexandraMendivelso <amendivelsolano@gmail.com>
2025-01-10 11:43:47 -0500

    Actualizacion modal patrocinadores

--- client/src/componentes/Patrocinadores/Patrocinadores.jsx
+++ client/src/componentes/Patrocinadores/Patrocinadores.jsx
@@ -1,52 +1,72 @@
+import { useState } from "react";
 import "./Patrocinadores.scss";
-import padre from "../../assets/Images/ImagesPatrocinadores/padre.svg"
-import madre from "../../assets/Images/ImagesPatrocinadores/madre.svg"
-import abuelo from "../../assets/Images/ImagesPatrocinadores/abuelo.svg"
-import abuela from "../../assets/Images/ImagesPatrocinadores/abuela.svg"
-import tio from "../../assets/Images/ImagesPatrocinadores/tio.svg"
-
 
 const Patrocinadores = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [patrocinadores, setPatrocinadores] = useState([]);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleSave = () => {
+    if (name.trim()) {
+      setPatrocinadores([...patrocinadores, { name, contributions: 0 }]);
+      setName("");
+      handleCloseModal();
+    }
+  };
+
   return (
     <div className="contenedorGeneralPatrocinadores">
       <div className="contenedorPatrocinadores">
         <h1>Patrocinadores</h1>
-        <button className="btnAgregarpatrocinador">Agregar</button>
+        <button className="btnAgregarpatrocinador" onClick={handleOpenModal}>
+          Agregar
+        </button>
       </div>
 
       <div className="contenedorPrincipalPatrocinadores">
-        <section className="patrocinadorIndividual">
-          <img src={padre} alt="" />
-          <span>Papa</span>
-          <span>Aportes</span>
-          <span>10</span>
-        </section>
-        <section className="patrocinadorIndividual">
-          <img src={madre} alt="" />
-          <span>Mama</span>
-          <span>Aportes</span>
-          <span>12</span>
-        </section>
-        <section className="patrocinadorIndividual">
-          <img src={abuela} alt="" />
-          <span>Abuela Lola</span>
-          <span>Aportes</span>
-          <span>5</span>
-        </section>
-        <section className="patrocinadorIndividual">
-          <img src={abuelo}alt="" />
-          <span>Abuelo Julio</span>
-          <span>Aportes</span>
-          <span>3</span>
-        </section>
-        <section className="patrocinadorIndividual">
-          <img src={tio} alt="" />
-          <span>Tio Cesar</span>
-          <span>Aportes</span>
-          <span>2</span>
-        </section>
+        {patrocinadores.map((patrocinador, index) => (
+          <section key={index} className="patrocinadorIndividual">
+            <span>{patrocinador.name}</span>
+            <span>Cantidad de aportes</span>
+            <span>{patrocinador.contributions}</span>
+          </section>
+        ))}
       </div>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modalContent">
+            <button className="closeButton" onClick={handleCloseModal}>
+              ×
+            </button>
+            <h2>Agregar Patrocinador</h2>
+            <label>
+              Nombre:
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Sebastian Mendivelso"
+              />
+            </label>
+            <div className="modalButtons">
+              <button className="btnGuardar" onClick={handleSave}>
+                Guardar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+
 export default Patrocinadores;
